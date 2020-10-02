6c521abc1553a14659050a76a5cf9460c87c1c2a
Danielle Stead <daniellestead@hotmail.co.nz>
2020-10-02 18:32:38 +1300

    Get rid of the silly things and make tomi small and fast

--- src/main.rs
+++ src/main.rs
@@ -10,90 +10,96 @@ fn main() {
                 ..Default::default()
             })
         .add_default_plugins()
-        .add_resource(Scoreboard { score: 0 })
+        // .add_resource(Scoreboard { score: 0 })
         .add_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)))
         .add_startup_system(setup.system())
-        .add_system(animate_system.system())
-        .add_system(scoreboard_system.system())
+        // .add_system(animate_system.system())
+        // .add_system(scoreboard_system.system())
         .add_system(movement_system.system())
         .run();
 }
 
-struct Scoreboard {
-    score: usize,
-}
+// struct Scoreboard {
+//     score: usize,
+// }
 
 struct Tomi {
     speed: f32,
 }
 
-fn animate_system(
-    texture_atlases: Res<Assets<TextureAtlas>>,
-    mut scoreboard: ResMut<Scoreboard>,
-    mut query: Query<(&mut Timer, &mut TextureAtlasSprite, &Handle<TextureAtlas>)>,
-) {
-    for (timer, mut sprite, texture_atlas_handle) in &mut query.iter() {
-        if timer.finished {
-            let texture_atlas = texture_atlases.get(&texture_atlas_handle).unwrap();
-            sprite.index = ((sprite.index as usize + 1) % texture_atlas.textures.len()) as u32;
-            scoreboard.score += 1;
-        }
-    }
-}
+// fn animate_system(
+//     texture_atlases: Res<Assets<TextureAtlas>>,
+//     mut scoreboard: ResMut<Scoreboard>,
+//     mut query: Query<(&mut Timer, &mut TextureAtlasSprite, &Handle<TextureAtlas>)>,
+// ) {
+//     for (timer, mut sprite, texture_atlas_handle) in &mut query.iter() {
+//         if timer.finished {
+//             let texture_atlas = texture_atlases.get(&texture_atlas_handle).unwrap();
+//             sprite.index = ((sprite.index as usize + 1) % texture_atlas.textures.len()) as u32;
+//             scoreboard.score += 1;
+//         }
+//     }
+// }
 
-fn scoreboard_system(scoreboard: Res<Scoreboard>, mut query: Query<&mut Text>) {
-    for mut text in &mut query.iter() {
-        text.value = format!("Score: {}", scoreboard.score);
-    }
-}
+// fn scoreboard_system(scoreboard: Res<Scoreboard>, mut query: Query<&mut Text>) {
+//     for mut text in &mut query.iter() {
+//         text.value = format!("Score: {}", scoreboard.score);
+//     }
+// }
 
 fn setup(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
-    mut textures: ResMut<Assets<Texture>>,
-    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+    // mut textures: ResMut<Assets<Texture>>,
+    // mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
-    let texture_handle = asset_server
-        .load_sync(
-            &mut textures,
-            "assets/blow_kiss.png",
-        )
-        .unwrap();
-    let texture = textures.get(&texture_handle).unwrap();
-    let texture_atlas = TextureAtlas::from_grid(texture_handle, texture.size, 3, 1);
-    let texture_atlas_handle = texture_atlases.add(texture_atlas);
+    // let texture_handle = asset_server
+    //     .load_sync(
+    //         &mut textures,
+    //         "assets/blow_kiss.png",
+    //     )
+    //     .unwrap();
+    let texture_handle = asset_server.load("assets/tomi.png").unwrap();
+    // let texture = textures.get(&texture_handle).unwrap();
+    // let texture_atlas = TextureAtlas::from_grid(texture_handle, texture.size, 3, 1);
+    // let texture_atlas_handle = texture_atlases.add(texture_atlas);
     commands
         .spawn(Camera2dComponents::default())
         .spawn(UiCameraComponents::default())
-        .spawn(SpriteSheetComponents {
-            texture_atlas: texture_atlas_handle,
-            transform: Transform::from_scale(1.0),
-            ..Default::default()
-        })
         // Tomi
-        .with(Tomi { speed: 500.0 })
-        .with(Timer::from_seconds(0.5, true))
+        .spawn(SpriteComponents {
+                     material: materials.add(texture_handle.into()),
+                     ..Default::default()
+        })
+        // .spawn(SpriteSheetComponents {
+        //     texture_atlas: texture_atlas_handle,
+        //     transform: Transform::from_scale(1.0),
+        //     ..Default::default()
+        // })
+        .with(Tomi { speed: 500.0 });
+        // .with(Timer::from_seconds(0.5, true))
         // Scoreboard
-        .spawn(TextComponents {
-            text: Text {
-                font: asset_server.load("assets/fonts/FiraSans-Bold.ttf").unwrap(),
-                value: "Score:".to_string(),
-                style: TextStyle {
-                    color: Color::rgb_u8(254, 209, 250),
-                    font_size: 40.0,
-                },
-            },
-            style: Style {
-                position_type: PositionType::Absolute,
-                position: Rect {
-                    top: Val::Px(5.0),
-                    left: Val::Px(5.0),
-                    ..Default::default()
-                },
-                ..Default::default()
-            },
-            ..Default::default()
-        });
+        // .spawn(TextComponents {
+        //     text: Text {
+        //         font: asset_server.load("assets/fonts/FiraSans-Bold.ttf").unwrap(),
+        //         value: "Score:".to_string(),
+        //         style: TextStyle {
+        //             color: Color::rgb_u8(254, 209, 250),
+        //             font_size: 40.0,
+        //         },
+        //     },
+        //     style: Style {
+        //         position_type: PositionType::Absolute,
+        //         position: Rect {
+        //             top: Val::Px(5.0),
+        //             left: Val::Px(5.0),
+        //             ..Default::default()
+        //         },
+        //         ..Default::default()
+        //     },
+        //     ..Default::default()
+        // });
 
 }
 
